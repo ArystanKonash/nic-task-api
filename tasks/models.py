0b867e13432c9fba05e4f226c07df32e95df1b4d
arystankonash <arsan65konahs@gmail.com>
2025-11-05 13:07:49 +0500

    fix: make description field optional in Task model

--- tasks/models.py
+++ tasks/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
     )
 
     title = models.CharField(max_length=100)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
